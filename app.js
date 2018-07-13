a0d0b30eb042423d8f4f2af32e1825b22441f119
Gofy83 <norbert.kovacs@hu.ibm.com>
2018-07-13 16:05:13 +0200

    DOM  EXAMINATION

---
+++ app.js
@@ -0,0 +1,22 @@
+let val;
+
+//EXAMINING THE DOM PROPERTIES
+
+val = document;
+val = document.all;
+val = document.all[0];
+val = document.all.length;
+val = document.head;
+val = document.domain;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+
+val = document.links;
+val = document.links[0].className;
+val = document.links[0].classList;
+
+
+
+console.log(val);
