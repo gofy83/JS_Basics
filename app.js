703abe34476881ed145ef971e93f2e41a1c925ca
gofy83 <40800333+gofy83@users.noreply.github.com>
2018-07-15 12:00:06 +0200

    single querySelector

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let val;
+/*let val;
 
 //EXAMINING THE DOM PROPERTIES
 
@@ -20,3 +20,34 @@ val = document.links[0].classList;
 
 
 console.log(val);
+
+
+// SINGLE DOM SELECTORS
+//document.getElementById()
+console.log(document.getElementById('task-title'));
+
+//Get things from the elementa
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+const taskTitle = document.getElementById('task-title');
+
+//Change styling
+//document.getElementById('task-title').style.background = '#333';
+//document.getElementById('task-title').style.color = '#fff';
+//document.getElementById('task-title').style.padding = '5px';
+//document.getElementById('task-title').style.display = 'none';
+
+//CHANGE THE CONTENT
+taskTitle.textContent = 'My Tasks';
+taskTitle.innerText = 'Open Tasks';
+taskTitle.innerHTML = '<span>Task List</span>';
+
+
+// document.querySelecotr()
+console.log(document.querySelector('#task-title'));
+//selecting on class
+console.log(document.querySelector('.card-title'));
+//selecting on element
+console.log(document.querySelector('h5'));
+*/
